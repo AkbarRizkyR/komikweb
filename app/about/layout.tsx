8a81c6a44d3c3e9a466e637619aff4cdedaff2fb
Akbar Rizky Rabbani <39977486+AkbarRizkyR@users.noreply.github.com>
2024-04-16 10:42:41 +0000

    Fixin

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,13 +1,52 @@
-export default function AboutLayout({
+import "@/styles/globals.css";
+import { Metadata } from "next";
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/config/fonts";
+import { Providers } from "@/app/providers";
+import { Navbar } from "@/components/navbar";
+import { Link } from "@nextui-org/link";
+import clsx from "clsx";
+
+export const metadata: Metadata = {
+	title: {
+		default: siteConfig.name,
+		template: `%s - ${siteConfig.name}`,
+	},
+	description: siteConfig.description,
+	themeColor: [
+		{ media: "(prefers-color-scheme: light)", color: "white" },
+		{ media: "(prefers-color-scheme: dark)", color: "black" },
+	],
+	icons: {
+		icon: "/logo.png",
+		shortcut: "/favicon-16x16.png",
+		apple: "/apple-touch-icon.png",
+	},
+};
+
+export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
 	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				{children}
-			</div>
-		</section>
+		<html lang="en" suppressHydrationWarning>
+			<head />
+			<body
+				className={clsx(
+					"min-h-screen bg-background font-sans antialiased",
+					fontSans.variable
+				)}
+			>
+				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+					<div className="relative flex flex-col h-screen">
+						<Navbar />
+						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+							{children}
+						</main>
+					</div>
+				</Providers>
+			</body>
+		</html>
 	);
 }
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,19 @@
-import { title } from "@/components/primitives";
+import { Card, CardBody } from '@nextui-org/card';
+import { Button } from '@nextui-org/button';
 
 export default function AboutPage() {
 	return (
-		<div>
-			<h1 className={title()}>About</h1>
+		<div className="w-full">
+			<div className="flex justify-center">
+				<h1 className="flex justify-center text-4xl font-bold">About Me!</h1>
+			</div>
+			<div>
+				<Card>
+					<CardBody>
+						
+					</CardBody>
+				</Card>
+			</div>
 		</div>
 	);
 }
